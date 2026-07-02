a9b881dfd4df47ca3c1f75db1f92a4f553ae38f5
kevzs <orlando.kj24@gmail.com>
2026-07-01 23:31:33 -0500

    Initial commit: Expo app scaffold with expo-router and NativeWind

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text className='text-3xl bg-slate-200'>App</Text>
+    </View>
+  )
+}
+
+export default App
